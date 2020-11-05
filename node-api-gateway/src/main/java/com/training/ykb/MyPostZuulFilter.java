fa4952b56f28d7e489abd8edd716dfc65ce48f7c
Osman <osman.yaycioglu@gmail.com>
2020-11-05 12:34:49 +0300

    day 333

---
+++ node-api-gateway/src/main/java/com/training/ykb/MyPostZuulFilter.java
@@ -0,0 +1,36 @@
+package com.training.ykb;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class MyPostZuulFilter extends ZuulFilter {
+
+    @Override
+    public boolean shouldFilter() {
+        HttpServletRequest requestLoc = RequestContext.getCurrentContext()
+                                                      .getRequest();
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        System.out.println("POST FILTER");
+        return null;
+    }
+
+    @Override
+    public String filterType() {
+        return FilterConstants.POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+}
---
+++ node-api-gateway/src/main/java/com/training/ykb/MyPreZuulFilter.java
@@ -0,0 +1,36 @@
+package com.training.ykb;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class MyPreZuulFilter extends ZuulFilter {
+
+    @Override
+    public boolean shouldFilter() {
+        HttpServletRequest requestLoc = RequestContext.getCurrentContext()
+                                                      .getRequest();
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        System.out.println("PRE FILTER");
+        return null;
+    }
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+}
